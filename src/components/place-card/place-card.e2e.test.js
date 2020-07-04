1165b7e4defc5dfdc9e98aac5e82f65ea4d7d6ea
oaglazunova <oaglazunova@gmail.com>
2020-07-05 00:08:46 +0300

    module3-task1 // card component test updated

--- src/components/place-card/place-card.e2e.test.js
+++ src/components/place-card/place-card.e2e.test.js
@@ -9,29 +9,32 @@ describe(`Place Card component`, () => {
   it(`callback for title is received`, () => {
     const handleTitleClick = jest.fn();
 
-    const main = shallow(
+    const card = shallow(
         <PlaceCard data={testData} handleTitleClick={handleTitleClick} handleCardHover={()=>{}} />
     );
 
-    main.find(`.place-card__name a`).forEach((node) => {
+    card.find(`.place-card__name a`).forEach((node) => {
       node.props().onClick();
     });
 
-    expect(handleTitleClick.mock.calls.length).toBe(main.find(`.place-card__name a`).length);
+    expect(handleTitleClick.mock.calls.length).toBe(card.find(`.place-card__name a`).length);
   });
   // Step 2: Test callback on hover
   it(`callback on hover is received`, () => {
     const handleCardHover = jest.fn();
 
-    const main = shallow(
+    const card = shallow(
         <PlaceCard data={testData} handleTitleClick={()=>{}} handleCardHover={handleCardHover} />
     );
 
-    main.find(`.place-card`).forEach((node) => {
-      node.props().onMouseEnter();
-    });
+    const cardElement = card.find(`.place-card`);
 
-    expect(handleCardHover.mock.calls.length).toBe(main.find(`.place-card`).length);
+    cardElement.props().onMouseEnter();
+    cardElement.props().onMouseLeave();
+
+    expect(handleCardHover).toHaveBeenCalledTimes(2);
+    expect(handleCardHover).toHaveBeenCalledWith(501);
+    expect(handleCardHover).toHaveBeenLastCalledWith(null);
   });
   // Step 3: Unit test - data has a correct type
   it(`check the type of data`, () => {
